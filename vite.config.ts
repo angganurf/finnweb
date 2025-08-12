8e3aaec163f3b7f460797c4298d6755b1ca52178
Angga Nurf <31128877+angganurf@users.noreply.github.com>
2025-08-12 17:32:41 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/finnweb/',
   optimizeDeps: {
     exclude: [
       "same-runtime/dist/jsx-dev-runtime",
